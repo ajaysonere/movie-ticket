328287c10811dbd1fd8b1c0002ea478ce984c1f7
ajay sonere <ajaysonere786@gmail.com>
2026-03-17 20:30:41 +0530

    second

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ import express from 'express';
 export const registerUser = (req , res) => {
     try {
          const user = "ajay";
-         return ;
+         return 0;
     } catch (error) {
         return res.status(500).json({success:false , message:'something went wrong.'})
     }
